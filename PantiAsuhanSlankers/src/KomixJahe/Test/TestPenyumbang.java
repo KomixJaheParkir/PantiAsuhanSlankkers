3096bf94cd5992425108e2cfaf0eeeb780dc44af
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-21 11:49:55 +0700

    Test Penyumbang

--- PantiAsuhanSlankers/src/KomixJahe/Test/TestPenyumbang.java
+++ PantiAsuhanSlankers/src/KomixJahe/Test/TestPenyumbang.java
@@ -17,5 +17,20 @@ public class TestPenyumbang {
         Penyumbang pen2 = new Penyumbang("Vera", "2018/12/13", "6.000.000");
         Penyumbang pen3 = new Penyumbang("Saragi", "2018/12/13", "7.000.000");
         
+        // test insert
+        pen1.save();
+        pen2.save();
+        pen3.save();
+        // test update
+        pen2.setTanggalsumbangan("2018/12/13");
+        pen2.save();
+        // test delete
+        //pen3.delete();
+        
+        // test select all
+        for(Penyumbang p : new Penyumbang().getAll())
+        {
+            System.out.println("Nama Penyumbang: " + p.getNama()+ ", Tanggal Sumbangan: " + p.getTanggalsumbangan()+ ", Jumlah Sumbangan: " + "Rp. " + p.getJumlahsumbangan());
+        }
     }
 }
