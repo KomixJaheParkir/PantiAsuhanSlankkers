208757ebb0d757526f9a17f86cd85337fa113618
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-21 11:49:28 +0700

    Test Penyumbang

---
+++ PantiAsuhanSlankers/src/KomixJahe/Test/TestPenyumbang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KomixJahe.Test;
+
+import KomixJahe.Other.Penyumbang;
+
+/**
+ *
+ * @author ASUS 10.1
+ */
+public class TestPenyumbang {
+    public static void main(String[] args) {
+        Penyumbang pen1 = new Penyumbang("Mike", "2018/12/13", "5.000.000");
+        Penyumbang pen2 = new Penyumbang("Vera", "2018/12/13", "6.000.000");
+        Penyumbang pen3 = new Penyumbang("Saragi", "2018/12/13", "7.000.000");
+        
+    }
+}
