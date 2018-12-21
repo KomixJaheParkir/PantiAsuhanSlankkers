2897710013c6e64771c235e5bcfc8ce843a13d0b
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-21 10:18:03 +0700

    Menambahkan Method getbyid

--- PantiAsuhanSlankers/src/KomixJahe/Pegawai/Penyumbang.java
+++ PantiAsuhanSlankers/src/KomixJahe/Pegawai/Penyumbang.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package KomixJahe.Pegawai;
+import KomixJahe.Koneksi.Koneksi;
 import java.util.ArrayList;
 import java.sql.*;
 /**
@@ -57,6 +58,27 @@ public class Penyumbang {
     public void setJumlahsumbangan(String jumlahsumbangan) {
         this.jumlahsumbangan = jumlahsumbangan;
     }
-
+     public Penyumbang getById(int id)
+    {
+        Penyumbang pen = new Penyumbang();
+        ResultSet rs = Koneksi.selectQuery("SELECT * FROM penyumbang " + "WHERE idpenyumbang = '" + id + "'");
+        
+        try
+        {
+            while(rs.next())
+            {
+                pen = new Penyumbang();
+                pen.setIdpenyumbang(rs.getInt("idpenyumbang"));
+                pen.setNama(rs.getString("nama"));
+                pen.setTanggalsumbangan(rs.getString("tanggalsumbangan"));
+                pen.setJumlahsumbangan(rs.getString("jumlahsumbangan"));
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return pen;
+    }
     
 }
