55a2b7d5f11d2f151487cabc7beddba9bd0d0826
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-21 10:44:12 +0700

    Menentukan Attribut dan konstruktor

--- PantiAsuhanSlankers/src/KomixJahe/Other/AnakAsuh.java
+++ PantiAsuhanSlankers/src/KomixJahe/Other/AnakAsuh.java
@@ -5,10 +5,30 @@
  */
 package KomixJahe.Other;
 
+import KomixJahe.Pegawai.Pegawai;
+
 /**
  *
  * @author ASUS 10.1
  */
 public class AnakAsuh {
+    private int idanak;
+    private Pegawai pegawai = new Pegawai();
+    private String namaanak;
+    private String alamat;
+    private String tglmasuk;
+    private String tglkeluar;
     
+     public AnakAsuh()
+    {
+        
+    }
+     public AnakAsuh(Pegawai pegawai, String nama, String alamat, String tglmasuk, String tglkeluar) {
+        this.pegawai = pegawai;
+        this.namaanak = nama;
+        this.alamat = alamat;
+        this.tglmasuk = tglmasuk;
+        this.tglkeluar = tglkeluar;
+    }
+
 }
