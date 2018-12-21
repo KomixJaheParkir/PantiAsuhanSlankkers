894e18126989c589f4388c80ed076b42d3fb29c3
FirmanCitraNurrahman <firmancitranurrahman@gmail.com>
2018-12-21 10:38:27 +0700

    membuat class

---
+++ PantiAsuhanSlankers/src/KomixJahe/Other/AnakAsuh.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KomixJahe.Other;
+
+/**
+ *
+ * @author ASUS 10.1
+ */
+public class AnakAsuh {
+    
+}
